a7621fb9cab6508f0f14090e6c5771d6f1a73ac3
Edwin Torres <CoachEd@gmail.com>
2017-07-24 14:31:16 -0400

    proper formation of host to ping Asterisk

--- server-db.js
+++ server-db.js
@@ -391,7 +391,8 @@ setImmediate(initialize);
 function sendResourceStatus() {
 	var hostMap = new Map();
 	// list of resources to check for status 
-	hostMap.set("Asterisk", decodeBase64(nconf.get('asteriskAD:sip:websocket')));
+	hostMap.set("Asterisk", "https://" + decodeBase64(nconf.get('asteriskAD:sip:realm')) + ":" + decodeBase64(nconf.get('asteriskAD:sip:wsport'))  + "/" );	
+	
 	hostMap.set("ACR-CDR", decodeBase64(nconf.get('acr-cdr:url')));
 	hostMap.set("VRS Lookup", decodeBase64(nconf.get('vrscheck:url')));
 	if (decodeBase64(nconf.get('environment')) === "ACL")
